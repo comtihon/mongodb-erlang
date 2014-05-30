82a68c1964ddca4eea9f4df57e17170b38b8c1f2
Valery Tikhonov <com.tihon@mail.ru>
2014-05-30 10:25:05 +0300

    small fixes in mc_action_man

--- src/core/mc_action_man.erl
+++ src/core/mc_action_man.erl
@@ -28,11 +28,11 @@ do(Connection, WriteMode, ReadMode, Database, Action) ->
 	Action(Connection).
 
 read(Connection, Request = #'query'{collection = Collection, batchsize = BatchSize} ) ->
-	{Cursor, Batch} = mc_connection_man:request(Connection, Request#'query'{slaveok = ReadMode =:= slave_ok}),
+	{Cursor, Batch} = mc_connection_man:request(Connection, Request),
 	mongo_cursor:create(Connection, Database, Collection, Cursor, BatchSize, Batch).  %TODO when pool Coonection will be {atom, PoolName}!! Resolv me
 %TODO cursor, what is it 0_o
 read_one(Connection, Request) ->
-	{0, Docs} = mc_connection_man:request(Connection, Request#'query'{batchsize = -1, slaveok = ReadMode =:= slave_ok}),
+	{0, Docs} = mc_connection_man:request(Connection, Request#'query'{batchsize = -1}),
 	case Docs of
 		[] -> {};
 		[Doc | _] -> {Doc}
