98825a1db6e46a2839b71528adf6c320caf9e52a
Valery Tikhonov <com.tihon@mail.ru>
2014-06-06 23:42:21 +0000

    made killcursor

--- src/connection/mc_worker.erl
+++ src/connection/mc_worker.erl
@@ -72,7 +72,7 @@ handle_call(Request, From, State = #state{socket = Socket, ets = Ets, conn_state
 	RespFun = fun(Response) -> gen_server:reply(From, Response) end,  % save function, which will be called on response
 	true = ets:insert_new(Ets, {Id, RespFun}),
 	{noreply, State};
-handle_call({request, Request}, _, State = #state{socket = Socket, conn_state = ConnState}) -> % TODO made - killcursor!
+handle_call(Request = #killcursor{}, _, State = #state{socket = Socket, conn_state = ConnState}) ->
 	{ok, _} = mc_worker_logic:make_request(Socket, ConnState#conn_state.database, Request),
 	{reply, ok, State};
 handle_call({stop, _}, _From, State) -> % stop request
