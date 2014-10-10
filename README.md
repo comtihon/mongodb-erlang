31cec6519d8fbd3df4327a337b83c8544dbf1a48
Valery Tikhonov <com.tihon@mail.ru>
2014-10-10 23:32:47 +0000

    added update test and readme

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
-This is the MongoDB driver for Erlang. [MongoDB](http://www.mongodb.org) is a document-oriented database management system. A driver is a client library that provides an API for connecting to MongoDB servers, performing queries and updates on those servers, and performing administrative tasks like creating indexes and viewing statistics.
-
-This version of the driver supports connecting to a single server or replica set, and pooling of both types of connections. Both connection types and pools are thread-safe, i.e. multiple processes can use the same connection/pool simultaneously without interfering with each other.
-
-This driver is implemented as an Erlang application named *mongodb*. It depends on another Erlang library application named [*bson*](http://github.com/mongodb/bson-erlang), which defines the document type and its standard binary representation. You need both of these. Below we describe the mongodb application; you should also see the bson application to understand the document type.
+This is the MongoDB driver for Erlang. [MongoDB](http://www.mongodb.org) is a document-oriented database management system.
+A driver is a client library that provides an API for connecting to MongoDB servers, performing queries and updates on 
+those servers, and performing administrative tasks like creating indexes and viewing statistics. This version of the driver 
+supports connecting to a single server or replica set, and pooling of both types of connections. Both connection types 
+and pools are thread-safe, i.e. multiple processes can use the same connection/pool simultaneously without interfering 
+with each other. This driver is implemented as an Erlang application named *mongodb*. It depends on another Erlang library 
+application named [*bson*](http://github.com/mongodb/bson-erlang), which defines the document type and its standard binary 
+representation. You need both of these. Below we describe the mongodb application; you should also see the bson application 
+to understand the document type.
 
 ### Installing
-
 Download and compile the application
 
 	$ git clone git://github.com/mongodb/mongodb-erlang.git mongodb
@@ -13,7 +16,6 @@ Download and compile the application
 	$ make
 
 ### Starting
-
 The bson application needs to be started before starting mongodb application
 
     > application:start (bson).
@@ -22,11 +24,12 @@ The mongodb application needs be started before using (to initialize an internal
 
 	> application:start (mongodb).
 
-Although the mongodb application includes several modules, you should only need to use *mongo*, which is the top-level interface for the driver. Likewise, you should only need to use the *bson* module in the bson application.
+Although the mongodb application includes several modules, you should only need to use *mongo*, which is the top-level 
+interface for the driver. Likewise, you should only need to use the *bson* module in the bson application.
 
 ### Connecting
-
-To connect to a database `test` on mongodb server listening on `localhost:27017` (or any address & port of your choosing) use `mongo:connect/3/4/6`.
+To connect to a database `test` on mongodb server listening on `localhost:27017` (or any address & port of your choosing) 
+use `mongo:connect/3/4/6`.
 
 	> Host = "127.0.0.1".
 	> Port = 27017.
@@ -37,15 +40,23 @@ To connect to a database `test` on mongodb server listening on `localhost:27017`
 
 `mongo:connect/4` also takes extra options as a param. It is `timeout` - time to wait for connection to database.  
 
-`mongo:connect/6` also adds read and write modes config. They are `master` and `slave_ok` for read and `unsafe`, `safe` and `{safe, bson:document()}` for write.  
+`mongo:connect/6` also adds read and write modes config. They are `master` and `slave_ok` for read and `unsafe`, `safe` 
+and `{safe, bson:document()}` for write.  
 
-`safe`, along with `{safe, GetLastErrorParams}` and `unsafe`, are write-modes. Safe mode makes a *getLastError* request after every write in the sequence. If the reply says it failed then the rest of the sequence is aborted and `mongo:do` returns `{failure, {write_failure, Reason}}`, or `{failure, not_master}` when connected to a slave. An example write failure is attempting to insert a duplicate key that is indexed to be unique. Alternatively, unsafe mode issues every write without a confirmation, so if a write fails you won't know about it and remaining operations will be executed. This is unsafe but faster because you there is no round-trip delay.
+`safe`, along with `{safe, GetLastErrorParams}` and `unsafe`, are write-modes. Safe mode makes a *getLastError* request 
+after every write in the sequence. If the reply says it failed then the rest of the sequence is aborted and `mongo:do` 
+returns `{failure, {write_failure, Reason}}`, or `{failure, not_master}` when connected to a slave. An example write 
+failure is attempting to insert a duplicate key that is indexed to be unique. Alternatively, unsafe mode issues every 
+write without a confirmation, so if a write fails you won't know about it and remaining operations will be executed. 
+This is unsafe but faster because you there is no round-trip delay.
 
-`master`, along with `slave_ok`, are read-modes. `master` means every query in the sequence must read fresh data (from a master/primary server). If the connected server is not a master then the first read will fail, the remaining operations will be aborted, and `mongo:do` will return `{failure, not_master}`. `slave_ok` means every query is allowed to read stale data from a slave/secondary (fresh data from a master is fine too).
+`master`, along with `slave_ok`, are read-modes. `master` means every query in the sequence must read fresh data (from 
+a master/primary server). If the connected server is not a master then the first read will fail, the remaining operations 
+will be aborted, and `mongo:do` will return `{failure, not_master}`. `slave_ok` means every query is allowed to read 
+stale data from a slave/secondary (fresh data from a master is fine too).
 
 
 ### Writing
-
 After you connected to your database - you can carry out write operations, such as `insert`, `update` and `delete`:
     
     > Collection = <<"test">>.
@@ -55,26 +66,28 @@ After you connected to your database - you can carry out write operations, such 
     		{name, <<"Phillies">>, home, {city, <<"Philadelphia">>, state, <<"PA">>}, league, <<"National">>},
     		{name, <<"Red Sox">>, home, {city, <<"Boston">>, state, <<"MA">>}, league, <<"American">>}
     	])
-An insert example (from `mongo_SUITE` test module). `Connection` is your Connection, got `from mongo:connect`, `Collection` is your collection name, `Doc` is something, you want to save.  
-Doc will be returned, if insert succeeded. If Doc doesn't contains `_id` field - an updated Doc will be returned - with automatically generated '_id' fields. If error occurred - Connection will fall.  
+An insert example (from `mongo_SUITE` test module). `Connection` is your Connection, got `from mongo:connect`, `Collection` 
+is your collection name, `Doc` is something, you want to save.  
+Doc will be returned, if insert succeeded. If Doc doesn't contains `_id` field - an updated Doc will be returned - with 
+automatically generated '_id' fields. If error occurred - Connection will fall.  
 
     > mongo:delete(Connection, Collection, Selector).
-Delete example. `Connection` is your Connection, `Collection` - is a collection you want to clean. `Selector` is the rules for cleaning. If you want to clean everything - pass empty `{}`.  
+Delete example. `Connection` is your Connection, `Collection` - is a collection you want to clean. `Selector` is the 
+rules for cleaning. If you want to clean everything - pass empty `{}`.  
 
 ### Reading
-
 To call read operations use `find`, `find_one`:
 
     > Cursor = mongo:find(Connection, Collection, Selector)
 All params similar to `delete`.  
-The difference between `find` and `find_one` is in return. Find_one just returns your result, while find returns you a `Cursor` - special process' pid. You can query data through the process with the help of `mc_cursor' module.  
+The difference between `find` and `find_one` is in return. Find_one just returns your result, while find returns you a 
+`Cursor` - special process' pid. You can query data through the process with the help of `mc_cursor' module.  
 
     > Result = mc_cursor:rest(Cursor),
     > mc_cursor:close(Cursor),
 __Important!__ Do not forget to close cursors after using them!
 
 ### Advance reading
-
 To search for params - specify `Selector`:
 
     mongo:find_one(Connection, Collection, {key, <<"123">>}).
@@ -93,23 +106,50 @@ will return one document from collection Collection with fetching `only` _id and
 will return your data without key and value params. If there is no other data - only _id will be returned.  
 __Important!__ For empty projector use `[]` instead `{}`. For empty selector use `{}`.
 
-### Creating indexes
+### Updating
+To add or update field in document - use `mongo:update` function with `$set` param.  
+This updates selected fields:  
+ 
+    Command = {'$set', {
+        quantity, 500,
+        details, {model, "14Q3", make, "xyz"},
+        tags, ["coats", "outerwear", "clothing"]
+    }},
+    mongo:update(Connection, Collection, {'_id', 100}, Command),
+This will add new field `expired`, if there is no such field, and set it to true.
 
+    Command = {'$set', {expired, true}},
+    mongo:update(Connection, Collection, {'_id', 100}, Command),
+This will update fields in nested documents. __Atoms using will be changed next version.__
+
+    Command = {'$set', {'details.make', "zzz"}},
+    mongo:update(Connection, Collection, {'_id', 100}, Command),
+This will update elements in array.  __Atoms using will be changed next version.__
+
+    Command = {'$set', {
+        'tags.1', "rain gear",
+        'ratings.0.rating', 2
+      }},
+    mongo:update(Connection, Collection, {'_id', 100}, Command),
+For result of executing this functions - see mongo_SUITE update test.
+
+### Creating indexes
 To create indexes - use `mongo:ensure_index/3` command:
 
     mongo:ensure_index(Connection, Collection, {key, {index, 1}}).  %simple
     mongo:ensure_index(Connection, Collection, {key, {index, 1}, name, <<"MyI">>}).  %advanced
     mongo:ensure_index(Connection, Collection, {key, {index, 1}, name, <<"MyI">>, unique, true, dropDups, true}).  %full
-ensure_index takes `mc_worker`' pid or atom name as first parameter, collection, where to create index, as second parameter and bson document with index
+ensure_index takes `mc_worker`' pid or atom name as first parameter, collection, where to create index, as second 
+parameter and bson document with index
 specification - as third parameter. In index specification one can set all or only some parameters.  
-If index specification is not full - it is automatically filled with values: `name, Name, unique, false, dropDups, false`, where `Name` is index's key.
+If index specification is not full - it is automatically filled with values: `name, Name, unique, false, dropDups, 
+false`, where `Name` is index's key.
 
 ### Administering
-
-This driver does not provide helper functions for commands. Use `mongo:command` directly and refer to the [MongoDB documentation](http://www.mongodb.org/display/DOCS/Commands) for how to issue raw commands.
+This driver does not provide helper functions for commands. Use `mongo:command` directly and refer to the 
+[MongoDB documentation](http://www.mongodb.org/display/DOCS/Commands) for how to issue raw commands.
 
 ### Authentication
-
 To authenticate use function `mongo:auth`.
 
     >  mongo:auth(C, <<"admin">>, <<"123456">>).
@@ -117,11 +157,13 @@ The return values will be:
 
 * `{false,{errmsg,<<"auth failed">>,code,18}}` in case of failure   
 * `{true,{dbname,<<"test">>,user,<<"admin">>}}` in case of success.  
-Plain Erlang string is interpreted as a BSON array of integers, so **make sure** to always encode your strings, as in `<<"hello">>` or `bson:utf8("hello")`.
+Plain Erlang string is interpreted as a BSON array of integers, so **make sure** to always encode your strings, as in 
+`<<"hello">>` or `bson:utf8("hello")`.
 
 ### Timeout
 
-By default timeout for all connections to connection gen_server is `infinity`. If you found problems with it - you can modify timeout.  
+By default timeout for all connections to connection gen_server is `infinity`. If you found problems with it - you can
+modify timeout.  
 To modify it just add `mc_worker_call_timeout` with new value to your applications's env config.
 
 ### Pooling
--- test/mongo_SUITE.erl
+++ test/mongo_SUITE.erl
@@ -7,156 +7,263 @@
 
 
 -export([
-	all/0,
-	init_per_suite/1,
-	end_per_suite/1,
-	init_per_testcase/2,
-	end_per_testcase/2,
-	search_and_query/1]).
+  all/0,
+  init_per_suite/1,
+  end_per_suite/1,
+  init_per_testcase/2,
+  end_per_testcase/2
+]).
 
 -export([
-	insert_and_find/1,
-	insert_and_delete/1
+  insert_and_find/1,
+  insert_and_delete/1,
+  search_and_query/1,
+  update/1
 ]).
 
 all() ->
-	[
-		insert_and_find,
-		insert_and_delete,
-		search_and_query
-	].
+  [
+    insert_and_find,
+    insert_and_delete,
+    search_and_query,
+    update
+  ].
 
 init_per_suite(Config) ->
-	application:start(bson),
-	application:start(mongodb),
-	[{database, test} | Config].
+  application:start(bson),
+  application:start(mongodb),
+  [{database, test} | Config].
 
 end_per_suite(_Config) ->
-	application:stop(mongodb),
-	application:stop(bson),
-	ok.
+  application:stop(mongodb),
+  application:stop(bson),
+  ok.
 
 init_per_testcase(Case, Config) ->
-	{ok, Connection} = mongo:connect("127.0.0.1", 27017, ?config(database, Config), safe, master, []),
-	[{connection, Connection}, {collection, collection(Case)} | Config].
+  {ok, Connection} = mongo:connect("127.0.0.1", 27017, ?config(database, Config), safe, master, []),
+  [{connection, Connection}, {collection, collection(Case)} | Config].
 
 end_per_testcase(_Case, Config) ->
-	Connection = ?config(connection, Config),
-	Collection = ?config(collection, Config),
-	mongo:delete(Connection, Collection, {}).
+  Connection = ?config(connection, Config),
+  Collection = ?config(collection, Config),
+  mongo:delete(Connection, Collection, {}).
 
 %% Tests
 insert_and_find(Config) ->
-	Connection = ?config(connection, Config),
-	Collection = ?config(collection, Config),
+  Connection = ?config(connection, Config),
+  Collection = ?config(collection, Config),
 
-	Teams = mongo:insert(Connection, Collection, [
-		{name, <<"Yankees">>, home, {city, <<"New York">>, state, <<"NY">>}, league, <<"American">>},
-		{name, <<"Mets">>, home, {city, <<"New York">>, state, <<"NY">>}, league, <<"National">>},
-		{name, <<"Phillies">>, home, {city, <<"Philadelphia">>, state, <<"PA">>}, league, <<"National">>},
-		{name, <<"Red Sox">>, home, {city, <<"Boston">>, state, <<"MA">>}, league, <<"American">>}
-	]),
-	4 = mongo:count(Connection, Collection, {}),
-	Teams2 = find(Connection, Collection, {}),
-	true = match_bson(Teams, Teams2),
+  Teams = mongo:insert(Connection, Collection, [
+    {name, <<"Yankees">>, home, {city, <<"New York">>, state, <<"NY">>}, league, <<"American">>},
+    {name, <<"Mets">>, home, {city, <<"New York">>, state, <<"NY">>}, league, <<"National">>},
+    {name, <<"Phillies">>, home, {city, <<"Philadelphia">>, state, <<"PA">>}, league, <<"National">>},
+    {name, <<"Red Sox">>, home, {city, <<"Boston">>, state, <<"MA">>}, league, <<"American">>}
+  ]),
+  4 = mongo:count(Connection, Collection, {}),
+  Teams2 = find(Connection, Collection, {}),
+  true = match_bson(Teams, Teams2),
 
-	NationalTeams = [Team || Team <- Teams, bson:at(league, Team) == <<"National">>],
-	NationalTeams2 = find(Connection, Collection, {league, <<"National">>}),
-	true = match_bson(NationalTeams, NationalTeams2),
+  NationalTeams = [Team || Team <- Teams, bson:at(league, Team) == <<"National">>],
+  NationalTeams2 = find(Connection, Collection, {league, <<"National">>}),
+  true = match_bson(NationalTeams, NationalTeams2),
 
-	2 = mongo:count(Connection, Collection, {league, <<"National">>}),
+  2 = mongo:count(Connection, Collection, {league, <<"National">>}),
 
-	TeamNames = [bson:include([name], Team) || Team <- Teams],
-	TeamNames = find(Connection, Collection, {}, {'_id', 0, name, 1}),
+  TeamNames = [bson:include([name], Team) || Team <- Teams],
+  TeamNames = find(Connection, Collection, {}, {'_id', 0, name, 1}),
 
 
-	BostonTeam = lists:last(Teams),
-	{BostonTeam2} = mongo:find_one(Connection, Collection, {home, {city, <<"Boston">>, state, <<"MA">>}}),
-	true = match_bson([BostonTeam], [BostonTeam2]).
+  BostonTeam = lists:last(Teams),
+  {BostonTeam2} = mongo:find_one(Connection, Collection, {home, {city, <<"Boston">>, state, <<"MA">>}}),
+  true = match_bson([BostonTeam], [BostonTeam2]).
 
 insert_and_delete(Config) ->
-	Connection = ?config(connection, Config),
-	Collection = ?config(collection, Config),
+  Connection = ?config(connection, Config),
+  Collection = ?config(collection, Config),
 
-	mongo:insert(Connection, Collection, [
-		{name, <<"Yankees">>, home, {city, <<"New York">>, state, <<"NY">>}, league, <<"American">>},
-		{name, <<"Mets">>, home, {city, <<"New York">>, state, <<"NY">>}, league, <<"National">>},
-		{name, <<"Phillies">>, home, {city, <<"Philadelphia">>, state, <<"PA">>}, league, <<"National">>},
-		{name, <<"Red Sox">>, home, {city, <<"Boston">>, state, <<"MA">>}, league, <<"American">>}
-	]),
-	4 = mongo:count(Connection, Collection, {}),
+  mongo:insert(Connection, Collection, [
+    {name, <<"Yankees">>, home, {city, <<"New York">>, state, <<"NY">>}, league, <<"American">>},
+    {name, <<"Mets">>, home, {city, <<"New York">>, state, <<"NY">>}, league, <<"National">>},
+    {name, <<"Phillies">>, home, {city, <<"Philadelphia">>, state, <<"PA">>}, league, <<"National">>},
+    {name, <<"Red Sox">>, home, {city, <<"Boston">>, state, <<"MA">>}, league, <<"American">>}
+  ]),
+  4 = mongo:count(Connection, Collection, {}),
 
-	mongo:delete_one(Connection, Collection, {}),
-	3 = mongo:count(Connection, Collection, {}).
+  mongo:delete_one(Connection, Collection, {}),
+  3 = mongo:count(Connection, Collection, {}).
 
 search_and_query(Config) ->
-	Connection = ?config(connection, Config),
-	Collection = ?config(collection, Config),
+  Connection = ?config(connection, Config),
+  Collection = ?config(collection, Config),
 
-	%insert test data
-	mongo:insert(Connection, Collection, [
-		{name, <<"Yankees">>, home, {city, <<"New York">>, state, <<"NY">>}, league, <<"American">>},
-		{name, <<"Mets">>, home, {city, <<"New York">>, state, <<"NY">>}, league, <<"National">>},
-		{name, <<"Phillies">>, home, {city, <<"Philadelphia">>, state, <<"PA">>}, league, <<"National">>},
-		{name, <<"Red Sox">>, home, {city, <<"Boston">>, state, <<"MA">>}, league, <<"American">>}
-	]),
+  %insert test data
+  mongo:insert(Connection, Collection, [
+    {name, <<"Yankees">>, home, {city, <<"New York">>, state, <<"NY">>}, league, <<"American">>},
+    {name, <<"Mets">>, home, {city, <<"New York">>, state, <<"NY">>}, league, <<"National">>},
+    {name, <<"Phillies">>, home, {city, <<"Philadelphia">>, state, <<"PA">>}, league, <<"National">>},
+    {name, <<"Red Sox">>, home, {city, <<"Boston">>, state, <<"MA">>}, league, <<"American">>}
+  ]),
 
-	%test selector
-	Res = find(Connection, Collection, {home, {city, <<"New York">>, state, <<"NY">>}}),
-	?debugFmt("Got ~p", [Res]),
-	[{'_id', _,
-		name, <<"Yankees">>, home,
-		{city, <<"New York">>, state, <<"NY">>},
-		league, <<"American">>},
-		{'_id', _,
-			name, <<"Mets">>, home,
-			{city, <<"New York">>, state, <<"NY">>},
-			league, <<"National">>}] = Res,
+  %test selector
+  Res = find(Connection, Collection, {home, {city, <<"New York">>, state, <<"NY">>}}),
+  ?debugFmt("Got ~p", [Res]),
+  [{'_id', _,
+    name, <<"Yankees">>, home,
+    {city, <<"New York">>, state, <<"NY">>},
+    league, <<"American">>},
+    {'_id', _,
+      name, <<"Mets">>, home,
+      {city, <<"New York">>, state, <<"NY">>},
+      league, <<"National">>}] = Res,
 
-	%test projector
-	Res2 = find(Connection, Collection, {home, {city, <<"New York">>, state, <<"NY">>}}, {name, true, league, true}),
-	?debugFmt("Got ~p", [Res2]),
-	[{'_id', _,
-		name, <<"Yankees">>,
-		league, <<"American">>},
-		{'_id', _,
-			name, <<"Mets">>,
-			league, <<"National">>}] = Res2,
+  %test projector
+  Res2 = find(Connection, Collection, {home, {city, <<"New York">>, state, <<"NY">>}}, {name, true, league, true}),
+  ?debugFmt("Got ~p", [Res2]),
+  [{'_id', _,
+    name, <<"Yankees">>,
+    league, <<"American">>},
+    {'_id', _,
+      name, <<"Mets">>,
+      league, <<"National">>}] = Res2,
+  Config.
 
-	Config.
+update(Config) ->
+  Connection = ?config(connection, Config),
+  Collection = ?config(collection, Config),
+
+  %insert test data
+  mongo:insert(Connection, Collection,
+    {'_id', 100,
+      sku, <<"abc123">>,
+      quantity, 250,
+      instock, true,
+      reorder, false,
+      details, {model, "14Q2", make, "xyz"},
+      tags, ["apparel", "clothing"],
+      ratings, [{by, "ijk", rating, 4}]}
+  ),
+
+  %check data inserted
+  Res = find(Connection, Collection, {'_id', 100}),
+  ?debugFmt("Got ~p", [Res]),
+  [{'_id', 100,
+    sku, <<"abc123">>,
+    quantity, 250,
+    instock, true,
+    reorder, false,
+    details, {model, "14Q2", make, "xyz"},
+    tags, ["apparel", "clothing"],
+    ratings, [{by, "ijk", rating, 4}]}] = Res,
+
+  %update existent fields
+  Command = {'$set', {
+    quantity, 500,
+    details, {model, "14Q3", make, "xyz"},
+    tags, ["coats", "outerwear", "clothing"]
+  }},
+  mongo:update(Connection, Collection, {'_id', 100}, Command),
+
+  %check data updated
+  Res1 = find(Connection, Collection, {'_id', 100}),
+  ?debugFmt("Got ~p", [Res1]),
+  [{'_id', 100,
+    sku, <<"abc123">>,
+    quantity, 500,
+    instock, true,
+    reorder, false,
+    details, {model, "14Q3", make, "xyz"},
+    tags, ["coats", "outerwear", "clothing"],
+    ratings, [{by, "ijk", rating, 4}]}] = Res1,
+
+  %update non existent fields
+  Command1 = {'$set', {expired, true}},
+  mongo:update(Connection, Collection, {'_id', 100}, Command1),
+
+  %check data updated
+  Res2 = find(Connection, Collection, {'_id', 100}),
+  ?debugFmt("Got ~p", [Res1]),
+  [{'_id', 100,
+    sku, <<"abc123">>,
+    quantity, 500,
+    instock, true,
+    reorder, false,
+    details, {model, "14Q3", make, "xyz"},
+    tags, ["coats", "outerwear", "clothing"],
+    ratings, [{by, "ijk", rating, 4}],
+    expired, true}
+  ] = Res2,
+
+  %update embedded fields
+  Command2 = {'$set', {'details.make', "zzz"}}, %TODO make bson avoid using atoms
+  mongo:update(Connection, Collection, {'_id', 100}, Command2),
+
+  %check data updated
+  Res3 = find(Connection, Collection, {'_id', 100}),
+  ?debugFmt("Got ~p", [Res1]),
+  [{'_id', 100,
+    sku, <<"abc123">>,
+    quantity, 500,
+    instock, true,
+    reorder, false,
+    details, {model, "14Q3", make, "zzz"},
+    tags, ["coats", "outerwear", "clothing"],
+    ratings, [{by, "ijk", rating, 4}],
+    expired, true}
+  ] = Res3,
+
+  %update list elements
+  Command3 = {'$set', { %TODO make bson avoid using atoms
+    'tags.1', "rain gear",
+    'ratings.0.rating', 2
+  }},
+  mongo:update(Connection, Collection, {'_id', 100}, Command3),
+  Res4 = find(Connection, Collection, {'_id', 100}),
+  ?debugFmt("Got ~p", [Res1]),
+  [{'_id', 100,
+    sku, <<"abc123">>,
+    quantity, 500,
+    instock, true,
+    reorder, false,
+    details, {model, "14Q3", make, "zzz"},
+    tags, ["coats", "rain gear", "clothing"],
+    ratings, [{by, "ijk", rating, 2}],
+    expired, true}
+  ] = Res4,
+
+  Config.
 
 
 %% @private
 find(Connection, Collection, Selector) ->
-	find(Connection, Collection, Selector, []).
+  find(Connection, Collection, Selector, []).
 
 find(Connection, Collection, Selector, Projector) ->
-	Cursor = mongo:find(Connection, Collection, Selector, Projector),
-	Result = mc_cursor:rest(Cursor),
-	mc_cursor:close(Cursor),
-	Result.
+  Cursor = mongo:find(Connection, Collection, Selector, Projector),
+  Result = mc_cursor:rest(Cursor),
+  mc_cursor:close(Cursor),
+  Result.
 
 %% @private
 collection(Case) ->
-	Now = now_to_seconds(erlang:now()),
-	<<(atom_to_binary(?MODULE, utf8))/binary, $-,
-	(atom_to_binary(Case, utf8))/binary, $-,
-	(list_to_binary(integer_to_list(Now)))/binary>>.
+  Now = now_to_seconds(erlang:now()),
+  <<(atom_to_binary(?MODULE, utf8))/binary, $-,
+  (atom_to_binary(Case, utf8))/binary, $-,
+  (list_to_binary(integer_to_list(Now)))/binary>>.
 
 %% @private
 now_to_seconds({Mega, Sec, _}) ->
-	(Mega * 1000000) + Sec.
+  (Mega * 1000000) + Sec.
 
 match_bson(Tuple1, Tuple2) when length(Tuple1) /= length(Tuple2) -> false;
 match_bson(Tuple1, Tuple2) ->
-	try
-		lists:foldr(
-			fun(Elem, Num) ->
-				Elem2 = lists:nth(Num, Tuple2),
-				Sorted = lists:sort(bson:fields(Elem)),
-				Sorted = lists:sort(bson:fields(Elem2))
-			end, 1, Tuple1)
-	catch
-		_:_ -> false
-	end,
-	true.
+  try
+    lists:foldr(
+      fun(Elem, Num) ->
+        Elem2 = lists:nth(Num, Tuple2),
+        Sorted = lists:sort(bson:fields(Elem)),
+        Sorted = lists:sort(bson:fields(Elem2))
+      end, 1, Tuple1)
+  catch
+    _:_ -> false
+  end,
+  true.
